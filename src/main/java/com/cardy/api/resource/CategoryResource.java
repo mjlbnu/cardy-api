79996250819f333074a31e6181e146d59f0d8516
mjlbnu <mjlbnu@gmail.com>
2021-06-03 23:41:01 -0300

    Has created new categories endpoints

--- src/main/java/com/cardy/api/resource/CategoryResource.java
+++ src/main/java/com/cardy/api/resource/CategoryResource.java
@@ -3,11 +3,14 @@ package com.cardy.api.resource;
 import com.cardy.api.model.Category;
 import com.cardy.api.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/categories")
@@ -18,6 +21,22 @@ public class CategoryResource {
 
     @GetMapping
     public List<Category> list() {
-        return  categoryRepository.findAll();
+        return categoryRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> create(@RequestBody Category category, HttpServletResponse response) {
+        Category savedCategory = categoryRepository.save(category);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+                .buildAndExpand(savedCategory.getId()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(savedCategory);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Category> findById(@PathVariable Long id) {
+        return categoryRepository.findById(id);
     }
 }
