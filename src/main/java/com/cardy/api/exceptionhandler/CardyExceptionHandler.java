e20d7800fbe67a475d3da2bd21856b65b309019a
mjlbnu <mjlbnu@gmail.com>
2021-06-04 23:03:37 -0300

    Feat(Exceptions) Has created a exceptionHandler

---
+++ src/main/java/com/cardy/api/exceptionhandler/CardyExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.cardy.api.exceptionhandler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CardyExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+                                                                  HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String userMessage = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
+        String devMessage = ex.getCause().toString();
+        return handleExceptionInternal(ex, new Error(userMessage, devMessage), headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    public static class Error {
+        private String userMessage;
+        private String devMessage;
+
+        public Error(String userMessage, String devMessage) {
+            this.userMessage = userMessage;
+            this.devMessage = devMessage;
+        }
+
+        public String getUserMessage() {
+            return userMessage;
+        }
+
+        public String getDevMessage() {
+            return devMessage;
+        }
+    }
+}
